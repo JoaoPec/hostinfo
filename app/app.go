2d5fc1b2112e08b18063e3b34a142560696ec212
JoaoPec <Joaopecurcino@hotmail.com>
2024-03-25 22:42:51 -0300

    add name server search functionality

--- app/app.go
+++ app/app.go
@@ -2,9 +2,9 @@ package app
 
 import (
 	"fmt"
+	"github.com/urfave/cli"
 	"log"
 	"net"
-	"github.com/urfave/cli"
 )
 
 func Generate() *cli.App {
@@ -12,28 +12,51 @@ func Generate() *cli.App {
 	app.Name = "hostinfo"
 	app.Usage = "Search ip's and server names"
 
+	flags := []cli.Flag{
+		cli.StringFlag{
+			Name:  "host",
+			Value: "google.com",
+		},
+	}
+
 	app.Commands = []cli.Command{
 
 		{
-			Name:  "ip",
-			Usage: "Search IP'S from adresses in the web",
-			Flags: []cli.Flag{
-				cli.StringFlag{
-					Name:  "host",
-					Value: "google.com",
-				},
-			},
+			Name:   "ip",
+			Usage:  "Search IP'S from adresses in the web",
+			Flags:  flags,
 			Action: searchIps,
 		},
+        {
+            Name: "server",
+            Usage: "Search server names from adresses in the web",
+            Flags: flags,
+            Action: searchServer,
+        },
 	}
 
 	return app
 }
 
+func searchServer(c *cli.Context){
+
+    host := c.String("host")
+
+    servers,err := net.LookupNS(host) // name server
+    if err != nil{
+        log.Fatal(err)
+    }
+
+    for _,server := range servers{
+        fmt.Println(server.Host)
+    }
+
+}
+
 func searchIps(c *cli.Context) {
 	host := c.String("host")
 
-	ips, err := net.LookupIP(host)
+	ips, err := net.LookupIP(host) //Ip
 	if err != nil {
 		log.Fatal(err)
 	}
